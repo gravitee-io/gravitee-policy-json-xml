04fe8d2354c3226e9b3368c3aeab93d818f0b55c
Cusnieux Guillaume <guillaume.cusnieux@graviteesource.com>
2021-02-01 11:25:42 +0100

    feat: create Json to xml policy (#1)
    feat: create Json to xml policy

---
+++ src/main/java/io/gravitee/policy/json2xml/JsonToXmlTransformationPolicy.java
@@ -0,0 +1,88 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.policy.json2xml;
+
+import io.gravitee.common.http.MediaType;
+import io.gravitee.gateway.api.Request;
+import io.gravitee.gateway.api.Response;
+import io.gravitee.gateway.api.buffer.Buffer;
+import io.gravitee.gateway.api.http.stream.TransformableRequestStreamBuilder;
+import io.gravitee.gateway.api.http.stream.TransformableResponseStreamBuilder;
+import io.gravitee.gateway.api.stream.ReadWriteStream;
+import io.gravitee.gateway.api.stream.exception.TransformationException;
+import io.gravitee.policy.api.annotations.OnRequestContent;
+import io.gravitee.policy.api.annotations.OnResponseContent;
+import io.gravitee.policy.json2xml.configuration.JsonToXmlTransformationPolicyConfiguration;
+import io.gravitee.policy.json2xml.configuration.PolicyScope;
+import io.gravitee.policy.json2xml.transformer.JSONObject;
+import io.gravitee.policy.json2xml.transformer.XML;
+import io.gravitee.policy.json2xml.utils.CharsetHelper;
+import java.nio.charset.Charset;
+import java.util.function.Function;
+import java.util.regex.Pattern;
+
+/**
+ * @author Guillaume Cusnieux (guillaume.cusnieux at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+public class JsonToXmlTransformationPolicy {
+
+    private static final String UTF8_CHARSET_NAME = "UTF-8";
+    private static final String CONTENT_TYPE = MediaType.APPLICATION_XML + ";charset=" + UTF8_CHARSET_NAME;
+
+    /**
+     * Json to xml transformation configuration
+     */
+    private final JsonToXmlTransformationPolicyConfiguration configuration;
+
+    public JsonToXmlTransformationPolicy(final JsonToXmlTransformationPolicyConfiguration configuration) {
+        this.configuration = configuration;
+    }
+
+    @OnResponseContent
+    public ReadWriteStream onResponseContent(Response response) {
+        if (configuration.getScope() == null || configuration.getScope() == PolicyScope.RESPONSE) {
+            Charset charset = CharsetHelper.extractFromContentType(response.headers().contentType());
+
+            return TransformableResponseStreamBuilder.on(response).contentType(CONTENT_TYPE).transform(map(charset)).build();
+        }
+        return null;
+    }
+
+    @OnRequestContent
+    public ReadWriteStream onRequestContent(Request request) {
+        if (configuration.getScope() == PolicyScope.REQUEST) {
+            Charset charset = CharsetHelper.extractFromContentType(request.headers().contentType());
+
+            return TransformableRequestStreamBuilder.on(request).contentType(CONTENT_TYPE).transform(map(charset)).build();
+        }
+        return null;
+    }
+
+    private Function<Buffer, Buffer> map(Charset charset) {
+        return input -> {
+            try {
+                String encodedPayload = new String(input.toString(charset).getBytes(UTF8_CHARSET_NAME));
+                JSONObject jsonPayload = new JSONObject(encodedPayload);
+                JSONObject jsonPayloadWithRoot = new JSONObject();
+                jsonPayloadWithRoot.append(this.configuration.getRootElement(), jsonPayload);
+                return Buffer.buffer(XML.toString(jsonPayloadWithRoot));
+            } catch (Exception ex) {
+                throw new TransformationException("Unable to transform JSON into XML: " + ex.getMessage(), ex);
+            }
+        };
+    }
+}
---
+++ src/test/java/utils/CharsetHelperTest.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package utils;
+
+import io.gravitee.common.http.MediaType;
+import io.gravitee.policy.json2xml.transformer.JSONObject;
+import io.gravitee.policy.json2xml.transformer.XML;
+import io.gravitee.policy.json2xml.utils.CharsetHelper;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author David BRASSELY (david.brassely at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+public class CharsetHelperTest {
+
+    @Test
+    public void shouldReturnDefaultCharset_noContentType() {
+        Charset charset = CharsetHelper.extractFromContentType(null);
+        Assert.assertEquals(StandardCharsets.UTF_8, charset);
+    }
+
+    @Test
+    public void shouldReturnDefaultCharset_contentType_withoutCharset() {
+        Charset charset = CharsetHelper.extractFromContentType(MediaType.APPLICATION_JSON);
+        Assert.assertEquals(StandardCharsets.UTF_8, charset);
+    }
+
+    @Test
+    public void shouldReturnDefaultCharset_contentType_withCharset() {
+        Charset charset = CharsetHelper.extractFromContentType(MediaType.APPLICATION_XML + ";charset=ISO-8859-1");
+        Assert.assertEquals("ISO-8859-1", charset.name());
+    }
+
+    @Test
+    public void shouldReturnDefaultCharset_contentType_withCharset_Capitalize() {
+        Charset charset = CharsetHelper.extractFromContentType(MediaType.APPLICATION_XML + "; Charset=ISO-8859-1");
+        Assert.assertEquals("ISO-8859-1", charset.name());
+    }
+
+    @Test
+    public void shouldReturnDefaultCharset_contentType_withCharset_Capitalize_quoted() {
+        Charset charset = CharsetHelper.extractFromContentType(MediaType.APPLICATION_XML + "; Charset=\"ISO-8859-1\"");
+        Assert.assertEquals("ISO-8859-1", charset.name());
+    }
+}

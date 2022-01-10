45be7cb3103b127e61bea174efd4c5f42ff2e025
Brassely David <brasseld@gmail.com>
2022-01-10 10:38:09 +0100

    feat(headers): Internal rework and introduce HTTP Headers API (#4)
    Closes gravitee-io/issues#6772

--- src/main/java/io/gravitee/policy/json2xml/utils/CharsetHelper.java
+++ src/main/java/io/gravitee/policy/json2xml/utils/CharsetHelper.java
@@ -15,6 +15,8 @@
  */
 package io.gravitee.policy.json2xml.utils;
 
+import io.gravitee.gateway.api.http.HttpHeaderNames;
+import io.gravitee.gateway.api.http.HttpHeaders;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.regex.Pattern;
@@ -28,6 +30,10 @@ public class CharsetHelper {
     private static final Charset UTF_8_CHARSET = StandardCharsets.UTF_8;
     private static final String CHARSET_TAG = "charset=";
     private static final Pattern CHARSET_PATTERN = Pattern.compile(Pattern.quote(CHARSET_TAG), Pattern.CASE_INSENSITIVE);
+
+    public static Charset extractCharset(HttpHeaders httpHeaders) {
+        return extractFromContentType(httpHeaders.get(HttpHeaderNames.CONTENT_TYPE));
+    }
 
     /**
      * Extract the charset from media-type as per https://tools.ietf.org/html/rfc7231#section-3.1.1.1

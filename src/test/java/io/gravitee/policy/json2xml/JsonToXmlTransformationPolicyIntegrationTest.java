9d422f4a56a5afa8be8b9d58cf06a079c565c737
Jeoffrey Haeyaert <jeoffrey.haeyaert@gmail.com>
2022-09-07 18:07:53 +0200

    feat: message level policy
    gravitee-io/issues#8403

---
+++ src/test/java/io/gravitee/policy/json2xml/JsonToXmlTransformationPolicyIntegrationTest.java
@@ -0,0 +1,113 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.policy.json2xml;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.github.tomakehurst.wiremock.matching.EqualToPattern;
+import io.gravitee.apim.gateway.tests.sdk.AbstractPolicyTest;
+import io.gravitee.apim.gateway.tests.sdk.annotations.DeployApi;
+import io.gravitee.apim.gateway.tests.sdk.annotations.GatewayTest;
+import io.gravitee.apim.gateway.tests.sdk.configuration.GatewayConfigurationBuilder;
+import io.gravitee.definition.model.Api;
+import io.gravitee.definition.model.ExecutionMode;
+import io.gravitee.policy.json2xml.configuration.JsonToXmlTransformationPolicyConfiguration;
+import io.reactivex.observers.TestObserver;
+import io.vertx.reactivex.core.buffer.Buffer;
+import io.vertx.reactivex.ext.web.client.HttpResponse;
+import io.vertx.reactivex.ext.web.client.WebClient;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Yann TAVERNIER (yann.tavernier at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+@GatewayTest
+public class JsonToXmlTransformationPolicyIntegrationTest
+    extends AbstractPolicyTest<JsonToXmlTransformationPolicy, JsonToXmlTransformationPolicyConfiguration> {
+
+    @Override
+    protected void configureGateway(GatewayConfigurationBuilder gatewayConfigurationBuilder) {
+        super.configureGateway(gatewayConfigurationBuilder);
+        gatewayConfigurationBuilder.set("api.jupiterMode.enabled", "true");
+    }
+
+    @Override
+    public void configureApi(Api api) {
+        api.setExecutionMode(ExecutionMode.JUPITER);
+    }
+
+    @Test
+    @DisplayName("Should post xml to backend")
+    @DeployApi("/apis/api-pre.json")
+    void shouldPostXmlContentToBackend(WebClient client) {
+        final String input = loadResource("/io/gravitee/policy/json2xml/input.json");
+        final String expected = loadResource("/io/gravitee/policy/json2xml/expected.xml");
+
+        wiremock.stubFor(post("/team").willReturn(ok()));
+
+        final TestObserver<HttpResponse<Buffer>> obs = client.post("/test").rxSendBuffer(Buffer.buffer(input)).test();
+
+        awaitTerminalEvent(obs)
+            .assertComplete()
+            .assertValue(
+                response -> {
+                    assertThat(response.statusCode()).isEqualTo(200);
+                    return true;
+                }
+            )
+            .assertNoErrors();
+
+        wiremock.verify(1, postRequestedFor(urlPathEqualTo("/team")).withRequestBody(new EqualToPattern(expected)));
+    }
+
+    @Test
+    @DisplayName("Should get xml from gateway")
+    @DeployApi("/apis/api-post.json")
+    void shouldGetXmlContentFromBackend(WebClient client) {
+        final String expected = loadResource("/io/gravitee/policy/json2xml/expected.xml");
+        final String backendResponse = loadResource("/io/gravitee/policy/json2xml/input.json");
+        wiremock.stubFor(get("/team").willReturn(ok(backendResponse)));
+
+        final TestObserver<HttpResponse<Buffer>> obs = client.get("/test").rxSend().test();
+
+        awaitTerminalEvent(obs)
+            .assertComplete()
+            .assertValue(
+                response -> {
+                    assertThat(response.statusCode()).isEqualTo(200);
+                    assertThat(response.bodyAsString()).isEqualTo(expected);
+                    return true;
+                }
+            )
+            .assertNoErrors();
+
+        wiremock.verify(1, getRequestedFor(urlPathEqualTo("/team")));
+    }
+
+    private String loadResource(String resource) {
+        try (InputStream is = this.getClass().getResourceAsStream(resource)) {
+            return new String(Objects.requireNonNull(is).readAllBytes(), StandardCharsets.UTF_8);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
--- src/test/java/io/gravitee/policy/json2xml/JsonToXmlTransformationPolicyTest.java
+++ src/test/java/io/gravitee/policy/json2xml/JsonToXmlTransformationPolicyTest.java
@@ -15,36 +15,40 @@
  */
 package io.gravitee.policy.json2xml;
 
+import static io.gravitee.policy.v3.json2xml.JsonToXmlTransformationPolicyV3.CONTENT_TYPE;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
-import io.gravitee.gateway.api.Request;
-import io.gravitee.gateway.api.Response;
 import io.gravitee.gateway.api.buffer.Buffer;
 import io.gravitee.gateway.api.http.HttpHeaderNames;
 import io.gravitee.gateway.api.http.HttpHeaders;
-import io.gravitee.gateway.api.stream.ReadWriteStream;
-import io.gravitee.policy.api.PolicyChain;
+import io.gravitee.gateway.api.stream.exception.TransformationException;
+import io.gravitee.gateway.jupiter.api.context.ExecutionContext;
+import io.gravitee.gateway.jupiter.api.context.Request;
+import io.gravitee.gateway.jupiter.api.context.Response;
+import io.gravitee.gateway.jupiter.api.message.DefaultMessage;
+import io.gravitee.gateway.jupiter.api.message.Message;
 import io.gravitee.policy.json2xml.configuration.JsonToXmlTransformationPolicyConfiguration;
-import io.gravitee.policy.json2xml.configuration.PolicyScope;
-import io.gravitee.reporter.api.http.Metrics;
+import io.reactivex.Completable;
+import io.reactivex.Maybe;
+import io.reactivex.MaybeTransformer;
+import io.reactivex.observers.TestObserver;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
-import java.time.Instant;
+import java.util.Objects;
+import java.util.function.Function;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
-import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 /**
- * @author Yann TAVERNIER (yann.tavernier at graviteesource.com)
+ * @author Jeoffrey HAEYAERT (jeoffrey.haeyaert at graviteesource.com)
  * @author GraviteeSource Team
  */
 @ExtendWith(MockitoExtension.class)
@@ -56,125 +60,184 @@ class JsonToXmlTransformationPolicyTest {
     private JsonToXmlTransformationPolicyConfiguration configuration;
 
     @Mock
-    private PolicyChain policyChain;
+    private ExecutionContext ctx;
 
-    @Spy
+    @Mock
     private Request request;
 
-    @Spy
+    @Mock
     private Response response;
+
+    @Captor
+    private ArgumentCaptor<MaybeTransformer<Buffer, Buffer>> onBodyCaptor;
+
+    @Captor
+    private ArgumentCaptor<Function<Message, Maybe<Message>>> onMessageCaptor;
 
     @BeforeEach
     public void setUp() {
         cut = new JsonToXmlTransformationPolicy(configuration);
+        lenient().when(ctx.request()).thenReturn(request);
+        lenient().when(ctx.response()).thenReturn(response);
+
+        lenient().when(request.headers()).thenReturn(HttpHeaders.create());
+        lenient().when(response.headers()).thenReturn(HttpHeaders.create());
     }
 
     @Test
-    @DisplayName("Should transform and add header OnRequestContent")
-    public void shouldTransformAndAddHeadersOnRequestContent() throws Exception {
-        String input = loadResource("/io/gravitee/policy/json2xml/input.json");
-        String expected = loadResource("/io/gravitee/policy/json2xml/expected.xml");
+    @DisplayName("Should transform and add header OnRequest")
+    public void shouldTransformAndAddHeadersOnRequest() throws Exception {
+        final String input = loadResource("/io/gravitee/policy/json2xml/input.json");
+        final String expected = loadResource("/io/gravitee/policy/json2xml/expected.xml");
+        final HttpHeaders headers = HttpHeaders.create();
 
-        // Prepare context
-        when(configuration.getScope()).thenReturn(PolicyScope.REQUEST);
+        when(request.onBody(onBodyCaptor.capture())).thenReturn(Completable.complete());
+        when(configuration.getRootElement()).thenReturn("root");
+        when(request.headers()).thenReturn(headers);
+
+        final TestObserver<Void> obs = cut.onRequest(ctx).test();
+        obs.assertNoValues();
+
+        final TestObserver<Buffer> bodyObs = ((Maybe<Buffer>) onBodyCaptor.getValue().apply(Maybe.just(Buffer.buffer(input)))).test();
+
+        bodyObs.assertValue(buffer -> expected.equals(buffer.toString()));
+        verifyHeaders(headers);
+    }
+
+    @Test
+    @DisplayName("Should do nothing when no body OnRequest")
+    public void shouldDoNothingWhenNoBodyOnRequest() {
+        when(request.onBody(onBodyCaptor.capture())).thenReturn(Completable.complete());
+
+        final TestObserver<Void> obs = cut.onRequest(ctx).test();
+        obs.assertNoValues();
+
+        final TestObserver<Buffer> bodyObs = ((Maybe<Buffer>) onBodyCaptor.getValue().apply(Maybe.empty())).test();
+
+        bodyObs.assertNoValues();
+        verify(request, never()).headers();
+    }
+
+    @Test
+    @DisplayName("Should interrupt with failure when bad json OnRequest")
+    public void shouldInterruptWhenBadJsonOnRequest() {
+        when(request.onBody(onBodyCaptor.capture())).thenReturn(Completable.complete());
+        when(request.headers()).thenReturn(HttpHeaders.create());
+
+        final TestObserver<Void> obs = cut.onRequest(ctx).test();
+        obs.assertNoValues();
+
+        final TestObserver<Buffer> bodyObs = ((Maybe<Buffer>) onBodyCaptor.getValue().apply(Maybe.just(Buffer.buffer("Bad Json")))).test();
+
+        bodyObs.assertError(TransformationException.class);
+    }
+
+    @Test
+    @DisplayName("Should transform and add header OnResponse")
+    public void shouldTransformAndAddHeadersOnResponse() throws Exception {
+        final String input = loadResource("/io/gravitee/policy/json2xml/input.json");
+        final String expected = loadResource("/io/gravitee/policy/json2xml/expected.xml");
+        final HttpHeaders headers = HttpHeaders.create();
+
+        when(response.onBody(onBodyCaptor.capture())).thenReturn(Completable.complete());
+        when(configuration.getRootElement()).thenReturn("root");
+        when(response.headers()).thenReturn(headers);
+
+        final TestObserver<Void> obs = cut.onResponse(ctx).test();
+        obs.assertNoValues();
+
+        final TestObserver<Buffer> bodyObs = ((Maybe<Buffer>) onBodyCaptor.getValue().apply(Maybe.just(Buffer.buffer(input)))).test();
+
+        bodyObs.assertValue(buffer -> expected.equals(buffer.toString()));
+        verifyHeaders(headers);
+    }
+
+    @Test
+    @DisplayName("Should do nothing when no body OnResponse")
+    public void shouldDoNothingWhenNoBodyOnResponse() throws Exception {
+        when(response.onBody(onBodyCaptor.capture())).thenReturn(Completable.complete());
+
+        final TestObserver<Void> obs = cut.onResponse(ctx).test();
+        obs.assertNoValues();
+
+        final TestObserver<Buffer> bodyObs = ((Maybe<Buffer>) onBodyCaptor.getValue().apply(Maybe.empty())).test();
+
+        bodyObs.assertNoValues();
+        verify(response, never()).headers();
+    }
+
+    @Test
+    @DisplayName("Should interrupt with failure when bad json OnResponse")
+    public void shouldInterruptWhenBadJsonOnResponse() {
+        when(response.onBody(onBodyCaptor.capture())).thenReturn(Completable.complete());
+        when(response.headers()).thenReturn(HttpHeaders.create());
+
+        final TestObserver<Void> obs = cut.onResponse(ctx).test();
+        obs.assertNoValues();
+
+        final TestObserver<Buffer> bodyObs = ((Maybe<Buffer>) onBodyCaptor.getValue().apply(Maybe.just(Buffer.buffer("Bad Json")))).test();
+
+        bodyObs.assertError(TransformationException.class);
+    }
+
+    @Test
+    @DisplayName("Should transform OnMessageRequest")
+    public void shouldTransformOnMessageRequest() throws Exception {
+        final String input = loadResource("/io/gravitee/policy/json2xml/input.json");
+        final String expected = loadResource("/io/gravitee/policy/json2xml/expected.xml");
+
+        when(request.onMessage(onMessageCaptor.capture())).thenReturn(Completable.complete());
         when(configuration.getRootElement()).thenReturn("root");
         when(request.headers()).thenReturn(HttpHeaders.create());
 
-        final ReadWriteStream result = cut.onRequestContent(request, policyChain);
-        assertThat(result).isNotNull();
-        result.bodyHandler(
-            resultBody -> {
-                assertResultingJsonObjectsAreEquals(expected, resultBody);
+        final TestObserver<Void> obs = cut.onMessageRequest(ctx).test();
+        obs.assertNoValues();
+
+        final TestObserver<Message> bodyObs = onMessageCaptor.getValue().apply(new DefaultMessage(input)).test();
+
+        bodyObs.assertValue(
+            message -> {
+                assertThat(expected.equals(message.content().toString())).isTrue();
+                verifyHeaders(message.headers());
+                return true;
             }
         );
-
-        result.write(Buffer.buffer(input));
-        result.end();
-
-        assertThat(request.headers().names()).contains(HttpHeaderNames.CONTENT_TYPE);
-        assertThat(request.headers().getAll(HttpHeaderNames.CONTENT_TYPE).get(0)).isEqualTo(JsonToXmlTransformationPolicy.CONTENT_TYPE);
-        assertThat(request.headers().names()).doesNotContain(HttpHeaderNames.TRANSFER_ENCODING);
-        assertThat(request.headers().names()).contains(HttpHeaderNames.CONTENT_LENGTH);
     }
 
     @Test
-    @DisplayName("Should not transform when TransformationException thrown OnRequestContent")
-    public void shouldNotTransformAndAddHeadersOnRequestContent() throws Exception {
-        String input = loadResource("/io/gravitee/policy/json2xml/invalid-input.json");
+    @DisplayName("Should transform OnMessageResponse")
+    public void shouldTransformOnMessageResponse() throws Exception {
+        final String input = loadResource("/io/gravitee/policy/json2xml/input.json");
+        final String expected = loadResource("/io/gravitee/policy/json2xml/expected.xml");
 
-        // Prepare context
-        when(configuration.getScope()).thenReturn(PolicyScope.REQUEST);
-        when(request.headers()).thenReturn(HttpHeaders.create());
-        when(request.metrics()).thenReturn(Metrics.on(Instant.now().toEpochMilli()).build());
-
-        final ReadWriteStream result = cut.onRequestContent(request, policyChain);
-        assertThat(result).isNotNull();
-
-        result.write(Buffer.buffer(input));
-        result.end();
-
-        assertThat(request.headers().names()).doesNotContain(HttpHeaderNames.CONTENT_TYPE);
-        assertThat(request.headers().names()).doesNotContain(HttpHeaderNames.TRANSFER_ENCODING);
-        assertThat(request.headers().names()).doesNotContain(HttpHeaderNames.CONTENT_LENGTH);
-        assertThat(request.metrics().getMessage()).contains("Unable to transform JSON into XML:");
-        verify(policyChain, times(1)).streamFailWith(any());
-    }
-
-    @Test
-    @DisplayName("Should transform and add header OnResponseContent")
-    public void shouldTransformAndAddHeadersOnResponseContent() throws Exception {
-        String input = loadResource("/io/gravitee/policy/json2xml/input.json");
-        String expected = loadResource("/io/gravitee/policy/json2xml/expected.xml");
-
-        // Prepare context
-        when(configuration.getScope()).thenReturn(PolicyScope.RESPONSE);
+        when(response.onMessage(onMessageCaptor.capture())).thenReturn(Completable.complete());
         when(configuration.getRootElement()).thenReturn("root");
         when(response.headers()).thenReturn(HttpHeaders.create());
 
-        final ReadWriteStream result = cut.onResponseContent(response, policyChain);
-        assertThat(result).isNotNull();
-        result.bodyHandler(
-            resultBody -> {
-                assertResultingJsonObjectsAreEquals(expected, resultBody);
+        final TestObserver<Void> obs = cut.onMessageResponse(ctx).test();
+        obs.assertNoValues();
+
+        final TestObserver<Message> bodyObs = onMessageCaptor.getValue().apply(new DefaultMessage(input)).test();
+
+        bodyObs.assertValue(
+            message -> {
+                assertThat(expected.equals(message.content().toString())).isTrue();
+                verifyHeaders(message.headers());
+                return true;
             }
         );
-
-        result.write(Buffer.buffer(input));
-        result.end();
-
-        assertThat(response.headers().names()).contains(HttpHeaderNames.CONTENT_TYPE);
-        assertThat(response.headers().getAll(HttpHeaderNames.CONTENT_TYPE).get(0)).isEqualTo(JsonToXmlTransformationPolicy.CONTENT_TYPE);
-        assertThat(response.headers().names()).doesNotContain(HttpHeaderNames.TRANSFER_ENCODING);
-        assertThat(response.headers().names()).contains(HttpHeaderNames.CONTENT_LENGTH);
     }
 
-    @Test
-    @DisplayName("Should not transform when TransformationException thrown OnResponseContent")
-    public void shouldNotTransformAndAddHeadersOnResponseContent() throws Exception {
-        String input = loadResource("/io/gravitee/policy/json2xml/invalid-input.json");
-
-        // Prepare context
-        when(configuration.getScope()).thenReturn(PolicyScope.RESPONSE);
-        when(response.headers()).thenReturn(HttpHeaders.create());
-
-        final ReadWriteStream result = cut.onResponseContent(response, policyChain);
-        assertThat(result).isNotNull();
-
-        result.write(Buffer.buffer(input));
-        result.end();
-
-        assertThat(response.headers().names()).doesNotContain(HttpHeaderNames.CONTENT_TYPE);
-        assertThat(response.headers().names()).doesNotContain(HttpHeaderNames.TRANSFER_ENCODING);
-        assertThat(response.headers().names()).doesNotContain(HttpHeaderNames.CONTENT_LENGTH);
-        verify(policyChain, times(1)).streamFailWith(any());
-    }
-
-    private void assertResultingJsonObjectsAreEquals(String expected, Object resultBody) {
-        assertThat(resultBody.toString()).isEqualTo(expected);
+    private void verifyHeaders(HttpHeaders headers) {
+        assertThat(headers.names()).contains(HttpHeaderNames.CONTENT_TYPE);
+        assertThat(headers.getAll(HttpHeaderNames.CONTENT_TYPE).get(0)).isEqualTo(CONTENT_TYPE);
+        assertThat(headers.names()).doesNotContain(HttpHeaderNames.TRANSFER_ENCODING);
+        assertThat(headers.names()).contains(HttpHeaderNames.CONTENT_LENGTH);
     }
 
     private String loadResource(String resource) throws IOException {
-        InputStream is = this.getClass().getResourceAsStream(resource);
-        return new String(is.readAllBytes(), StandardCharsets.UTF_8);
+        try (InputStream is = this.getClass().getResourceAsStream(resource)) {
+            return new String(Objects.requireNonNull(is).readAllBytes(), StandardCharsets.UTF_8);
+        }
     }
 }
